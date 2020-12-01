e7edc74c7b238418c5b676ad29541b249273477b
Ionela Chiriac <ionela.s.chiriac@gmail.com>
2020-12-01 20:36:24 +0200

    commit 1 dec dicee second part mod

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,14 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -27,9 +34,12 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Polcule! :))))');
+                setState(() {
+                  leftDiceNumber = 3;
+                  print('diceNumber = $leftDiceNumber');
+                });
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
