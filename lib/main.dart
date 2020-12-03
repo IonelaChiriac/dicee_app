7728f5f1ca2420c0131c235bd301846d0373a1bd
Ionela Chiriac <ionela.s.chiriac@gmail.com>
2020-12-03 21:29:22 +0200

    commit 3 dec dicee final part mod

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -25,6 +26,13 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +43,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
-                  leftDiceNumber = 3;
-                  print('diceNumber = $leftDiceNumber');
+                  changeDiceFace();
                 });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
@@ -45,9 +52,11 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Tot polcusol esti!!!!');
+                setState(() {
+                  changeDiceFace();
+                });
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
